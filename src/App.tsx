f58171ce012e352604f1d03a779c4bac88800c1a
Vladislav <typlomi@gmail.com>
2022-11-19 20:06:08 +0600

    Подготовка классов

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BoardComponent from './components/BoardComponent/BoardComponent';
+import './sass/style.scss';
+
+function App() {
+  return (
+    <div className='app'>
+      <BoardComponent />
+    </div>
+  );
+}
+
+export default App;
