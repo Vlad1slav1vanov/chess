ec7805eb10328427fb3ed50dcc562cec906af947
Vladislav <typlomi@gmail.com>
2022-11-19 20:25:12 +0600

    Отображение доски

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BoardComponent from './components/BoardComponent/BoardComponent';
+import { Board } from './models/Board';
 import './sass/style.scss';
 
 function App() {
+  const [board, setBoard] = useState(new Board());
+
+  useEffect(() => {
+    restart()
+  }, [])
+
+  function restart() {
+    const newBoard = new Board();
+    newBoard.initCells();
+    setBoard(newBoard);
+  }
+
   return (
     <div className='app'>
-      <BoardComponent />
+      <BoardComponent board={board} setBoard={setBoard}/>
     </div>
   );
 }
