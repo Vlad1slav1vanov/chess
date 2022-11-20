57dc900344a6fb4521bc7cdfd897ac75c0b7d8b0
Vladislav <typlomi@gmail.com>
2022-11-20 21:53:03 +0600

    Логика переключения игрока

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import BoardComponent from './components/BoardComponent/BoardComponent';
 import { Board } from './models/Board';
+import { Colors } from './models/Colors';
+import { Player } from './models/Player';
 import './sass/style.scss';
 
 function App() {
   const [board, setBoard] = useState(new Board());
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE));
+  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK));
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null)
 
   useEffect(() => {
     restart()
+    setCurrentPlayer(whitePlayer);
   }, [])
 
   function restart() {
@@ -17,9 +23,18 @@ function App() {
     setBoard(newBoard);
   }
 
+  function swapPlayer() {
+    setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer)
+  }
+
   return (
     <div className='app'>
-      <BoardComponent board={board} setBoard={setBoard}/>
+      <BoardComponent 
+        board={board} 
+        setBoard={setBoard}
+        currentPlayer={currentPlayer}
+        swapPlayer={swapPlayer}
+      />
     </div>
   );
 }
