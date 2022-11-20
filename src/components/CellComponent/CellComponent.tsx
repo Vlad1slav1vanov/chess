750a7cef91ca5c3ef6ac88e24d5e1c8d935f6e57
Vladislav <typlomi@gmail.com>
2022-11-20 19:09:34 +0600

    Логика Ферзя слона и ладьи

--- src/components/CellComponent/CellComponent.tsx
+++ src/components/CellComponent/CellComponent.tsx
@@ -3,11 +3,17 @@ import { Cell } from "../../models/Cell";
 
 interface CellProps {
   cell: Cell;
+  selected: boolean;
+  click: (cell: Cell) => void;
 }
 
-const CellComponent: FC<CellProps> = ({cell}) => {
+const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
   return (
-    <div className={['cell', cell.color].join(' ')}>
+    <div 
+      className={['cell', cell.color, selected ? "selected" : cell.available && cell.figure ? "available-figure" : ''].join(' ')}
+      onClick={() => click(cell)}
+    >
+      {cell.available && !cell.figure && <div className={"available"}></div>}
       {cell.figure?.logo && <img src={cell.figure.logo} alt="" />}
     </div>
   )
