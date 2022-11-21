54ab69998189b2abb9a03405e0e9ff4b673a86f1
Vladislav <typlomi@gmail.com>
2022-11-21 18:11:16 +0600

    замена картинок фигур и логика ходов короля

--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -10,11 +10,25 @@ export class King extends Figure {
     this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
     this.name = FigureNames.KING;
   }
-  
+
   canMove(target: Cell): boolean {
-    if (!super.canMove(target)) {
-      return false;
-    }
-    return true
+    if(!super.canMove(target)) return false;
+    const isVerticalMove = (target.y === this.cell.y + 1 || target.y === this.cell.y - 1) && target.x === this.cell.x;
+    const isHorizontalMove = (target.x === this.cell.x + 1 || target.x === this.cell.x - 1) && target.y === this.cell.y;
+    const isLeftDiagonal = (
+      (target.x === this.cell.x + 1 && target.y === this.cell.y + 1) 
+      || (target.x === this.cell.x - 1 && target.y === this.cell.y - 1)
+    )
+    const isRightDiagonal = (
+      (target.x === this.cell.x + 1 && target.y === this.cell.y - 1) 
+      || (target.x === this.cell.x - 1 && target.y === this.cell.y + 1)
+    )
+
+    if(isVerticalMove 
+      || isHorizontalMove 
+      || isLeftDiagonal 
+      || isRightDiagonal) return true;
+
+    return false;
   }
 }
